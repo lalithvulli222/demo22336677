d7312ba4248c87117c01318e0ba8379be80d2ffe
admin <lalithvulli222@gmail.com>
2019-11-09 22:29:53 +0530

    my code upload

---
+++ seleniumtesting/src/abcd/Class1.java
@@ -0,0 +1,15 @@
+package abcd;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10, b=20;	
+		char c = 'A';
+		double d= 123.456;
+		System.out.println(a+b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
